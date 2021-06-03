33b39e039999787be8db45e9f1d4dad674e0af70
Isra <israbr@gmail.com>
2021-06-03 20:32:07 +0200

    with footer

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:mestizo/messages/messages.i18n.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -11,9 +9,9 @@ class Landing extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        mainAxisSize: MainAxisSize.max,
         children: [
           Flexible(
+            flex: 3,
             child: Padding(
               padding: const EdgeInsets.all(40.0),
               child: Image(
@@ -23,14 +21,12 @@ class Landing extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            height: 60,
-          ),
+          Spacer(),
           Flexible(
             child: ElevatedButton(
               style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromRGBO(0, 116, 130, 1)),
+                      Color.fromRGBO(43, 57, 100, 1)),
                   padding:
                       MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(25)),
                   textStyle: MaterialStateProperty.all<TextStyle>(
@@ -39,16 +35,36 @@ class Landing extends StatelessWidget {
               child: Text(Messages().landing.delivery),
             ),
           ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Text(Messages().landing.delivery),
+          Spacer(
+            flex: 1,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  '955146497',
+                  style: _buildStyle(),
+                ),
+                Text(
+                  'Calle Peris Mencheta 31',
+                  style: _buildStyle(),
+                )
+              ],
             ),
           )
         ],
       ),
     );
   }
+
+  TextStyle _buildStyle() => TextStyle(
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.italic,
+        fontSize: 18,
+        color: Color.fromRGBO(0, 116, 130, 1),
+      );
 
   Future<bool> goToDeliDeli() async =>
       await launch('https://delideli.es/mestizo');
